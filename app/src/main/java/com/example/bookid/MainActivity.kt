fa409eacf35b2fc64092d242c02a62dc3b1c0441
Zahra Ghiasi <z.ghiac@gmail.com>
2024-08-14 00:59:39 +0330

    MainActivity.kt and resource errors are fixed

--- app/src/main/java/com/example/bookid/MainActivity.kt
+++ app/src/main/java/com/example/bookid/MainActivity.kt
@@ -16,32 +16,7 @@ import com.example.bookid.ui.theme.BookidTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            BookidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BookidTheme {
-        Greeting("Android")
-    }
-}
