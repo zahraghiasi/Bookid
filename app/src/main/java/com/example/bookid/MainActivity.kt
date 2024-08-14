05ac9666ba8e81000ba349a0d35801236f4fbc3c
Zahra Ghiasi <z.ghiac@gmail.com>
2024-08-14 23:08:19 +0330

    MainActivity connected to CharecterView

--- app/src/main/java/com/example/bookid/MainActivity.kt
+++ app/src/main/java/com/example/bookid/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.bookid
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,10 +17,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.bookid.ui.theme.BookidTheme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val charbtn1 = findViewById<ImageButton>(R.id.character1)
+        charbtn1.setOnClickListener {
+            Toast.makeText(applicationContext,"You clicked Submit Button.",Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, CharacterView::class.java))
+            finish();
+        }
     }
 }
 
